bbe8ca35c53e36a0ad7590194b8998492d920a63
Smeby33 <smebedoh33@gmail.com>
2025-02-07 15:40:53 +0100

    eco3

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./database");
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://smebedoh33:PjOL4Miphz4OI01z@eko.tslzc.mongodb.net/geslecdb?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connecté ✅'))
+.catch(err => console.error('Erreur de connexion à MongoDB ❌', err));
+
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'https://eko-ten.vercel.app' }));
 
 const userRoutes = require("./routes/userRoutes");
 const householdRoutes = require("./routes/householdRoutes");
@@ -18,5 +28,5 @@ app.use("/devices", deviceRoutes);
 app.use("/consumption", consumptionRoutes);
 
 app.listen(5000, () => {
-  console.log("🚀 Serveur en ligne sur http://localhost:5000");
+  console.log("🚀 Serveur en ligne sur https://eko-bak.onrender.com");
 });
